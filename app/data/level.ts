b0baa624b383b40e5c977ee202371813c07253e5
MswTester <mswtester10417@gmail.com>
2023-09-28 17:35:29 +0900

    fix level error

--- app/data/level.ts
+++ app/data/level.ts
@@ -1,17 +1,5 @@
 import { level } from "./types";
 
 export const levels:{[key:string]:level} = {
-    "test":{
-        backgroundColor:"#000000",
-        bpm:100,
-        events:[],
-        offset:0,
-        song:"assets/song/something.mp3",
-        volume:70,
-        endpoint:90,
-        objs:[],
-        position:[0, 0],
-        rotate:0,
-        scale:1,
-    }
+    "test":{"bpm":100,"events":[],"endpoint":90,"objs":[],"offset":0,"song":"","volume":100,"backgroundColor":"#000000","position":[0,0],"rotate":0,"scale":1,"filters":{"blur":0,"dot":0,"motionBlur":0,"bloom":0,"godray":0,"convolution":0,"glitch":0,"grayscale":0,"noise":0,"pixelate":0,"rgbsplit":0}}
 }
